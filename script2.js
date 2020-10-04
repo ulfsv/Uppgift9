f3590724167fe1eb750f6c2cabecd7a036b0bbfc
Ulf Svensson <ulf.e.sv@gmail.com>
2020-10-04 13:08:08 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -10,15 +10,17 @@ form.addEventListener('submit', (event) => {
     let form = document.getElementById('demo');
     let a = form.elements['slut'];
     let b = form.elements['bishvarde'];
+    let c = form.elements['boschvarde'];
     let end = parseInt(a.value);
     let bish = parseInt(b.value);
+    let bosch = parseInt(c.value);
 
     let start = 1;
   
     console.log(start + '<br/>');
     console.log(end + '<br/>');
     console.log(bish + '<br/>');
-
+    console.log(bosch + '<br/>');
   
     function range(start, end) {
         return Array(end - start + 1).fill().map((_, idx) => start + idx)
@@ -28,14 +30,14 @@ form.addEventListener('submit', (event) => {
       array.forEach(myFunction); 
       function myFunction(item, index) 
             { 
-            if ((item % 3 === 0) && (item % 4 === 0)) {
+            if ((item % bish === 0) && (item % bosch === 0)) {
                 console.log("Bish-Bosh");
                 document.write("Bish-Bosh" + '<br/>');  
                 
-            } else if (item %3 === 0) { 
+            } else if (item % bish === 0) { 
                 console.log("Bish");
                 document.write("Bish" + '<br/>');            
-            } else if (item % 4 === 0) {
+            } else if (item % bosch === 0) {
                 console.log("Bosh");
                 document.write("Bosh" + '<br/>');            
             } else {
